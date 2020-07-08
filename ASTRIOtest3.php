b01a45e383bbee0cd39c711ddb0d2e141965adc6
Anflame <67423444+Anflame@users.noreply.github.com>
2020-07-08 21:13:57 +0400

    Test3Version2

--- ASTRIOtest3.php
+++ ASTRIOtest3.php
@@ -1,29 +1,66 @@
 <?php
-function correctHTML($array){
-    $b = 0;
+function tagNewArray($array,$firstPosition,$tag, &$lastPosition){
+    if($lastPosition !== false){
+        if($lastPosition-$firstPosition != 1){
+            $newArray = array();
+            for ($j=$firstPosition; $j <= $lastPosition; $j++) {
+                $newArray[$j] = $array[$j];
+            }
+            $arrayValues = array_count_values($newArray);
+            if($arrayValues["</$tag>"] != $arrayValues["<$tag>"]){
+                $arraySliceLastTag = array_slice($array,$lastPosition+1,count($array), TRUE);
+                $lastPosition = array_search("</$tag>",$arraySliceLastTag);
+                if($lastPosition !== false){
+                    tagNewArray($array,$firstPosition,$tag,$lastPosition);
+                }
+                else $result = false;
+            }
+            else $result = true;
+        }
+        else $result = true;
+    }
+    else $result = false;
+return $result;
+}
+function correctHTML($array) {
+    $result = true;
     for($i=0;$i<count($array);$i++){
-        $a = str_split($array[$i]);
-        $seach = array("<","/",">");
-        if($a[1] != '/'){
-            $a = array_diff($a,$seach);
-            $a = implode($a);
-            if(!in_array("</$a>",$array)){
-                $b++;
+        $arrayValues = array_count_values($array);
+        $arrSplit = str_split($array[$i]);
+        $search = ["<","/",">"];
+        $arrayDiff = array_diff($arrSplit,$search);
+        $implodeTag = implode($arrayDiff);
+        if($arrayValues["<$implodeTag>"] != $arrayValues["</$implodeTag>"]){
+            if($arrayValues["<$implodeTag>"] > $arrayValues["</$implodeTag>"])
+                $result = false;
+            elseif($arrayValues["<$implodeTag>"] < $arrayValues["</$implodeTag>"])
+                $result = false;
+        }
+        else {
+            if($arrSplit[1] != '/'){
+                if($i == count($array)+1){
+                    $result = false;
+                }
+                else{
+                    $arraySlice = array_slice($array,$i,count($array),TRUE);
+                    $lastPosition = array_search("</$implodeTag>",$arraySlice);
+                    $resultTagNewArray = tagNewArray($array,$i,$implodeTag,$lastPosition);
+                    if($resultTagNewArray === false){
+                        $result = false;
+                    }
+                }
+            }
+            elseif($arrSplit[1] == '/'){
+                if($i === 0){
+                    $result = false;
+                }
             }
         }
-        elseif($a[1] == '/'){
-            $a = array_diff($a,$seach);
-            $a = implode($a);
-            if(!in_array("<$a>",$array)){
-                $b++;
-            } 
-        }
     }
-    if($b == 0){
-        echo "Корректный код HTML";
-    }
-    else  echo "Не корректный код HTML";
+    if($result == false) $result = "Некорректный HTML";
+    else $result = "Корректный HTML";
+    return $result;
 }
-$array = array("<a>","<div>","</div>","</a>","<b>","</b>");
+$array = ["<div>","<div>","<a>","</div>","<span>","<b>","</b>","<b>","</b>","</b>","<b>","</span>","</a>","</div>"];
 correctHTML($array);
 ?>
